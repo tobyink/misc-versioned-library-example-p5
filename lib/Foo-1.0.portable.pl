93bc11adef72fad65ef6c8724f1f011d0fc7a57c
Toby Inkster <mail@tobyinkster.co.uk>
2020-11-21 18:53:34 +0000

    having a class also named Foo is confusing

--- lib/Foo-1.0.portable.pl
+++ lib/Foo-1.0.portable.pl
@@ -4,11 +4,6 @@ use warnings;
 return {
 	version => '1.0',
 	class => {
-		'Foo' => {
-			has => {
-				'name' => { type => 'Str' },
-			},
-		},
 		'Bar' => {
 			has => {
 				'name' => { type => 'Str' },
--- lib/Foo-1.1.portable.pl
+++ lib/Foo-1.1.portable.pl
@@ -4,11 +4,6 @@ use warnings;
 return {
 	version => '1.1',
 	class => {
-		'Foo' => {
-			has => {
-				'name' => { type => 'Str' },
-			},
-		},
 		'Bar' => {
 			has => {
 				'name'        => { type => 'Str' },
